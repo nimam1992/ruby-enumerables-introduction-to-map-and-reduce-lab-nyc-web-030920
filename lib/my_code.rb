89a9589ef2798702fc8e1ee0819889580b2b6595
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-02 20:15:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,8 +3,10 @@ def map_to_negativize(source_array)
   negative_array = []
   while i<source_array.length do
     negative_array << source_array[i]*-1
+    i+=1
   end
   negative_array
   end
 
-  pp map_to_negativize([1,2,3,-9])
+pp map_to_negativize([1,2,3,-9])
+  
