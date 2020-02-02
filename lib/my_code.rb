d4f89e78e69de8d5ba49f60374cd3b1bd138acb3
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-02 20:27:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,9 +50,8 @@ def reduce_to_total(source_array)
 
 def reduce_to_total(starting_point,source_array)
   i = 0
-  new_array = []
   while i<source_array.length do
-    new_array << source_array[i]*-1
+    starting_point+=source_array[i]*-1
     i+=1
   end
   new_array
