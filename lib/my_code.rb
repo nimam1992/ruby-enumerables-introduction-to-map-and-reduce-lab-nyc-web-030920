4c320f34496fed2483c8089842be6050b18fb89f
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-02 20:17:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,12 @@
 def map_to_negativize(source_array)
   i = 0
-  negative_array = []
+  new_array = []
   while i<source_array.length do
-    negative_array << source_array[i]*-1
+    new_array << source_array[i]*-1
     i+=1
   end
-  negative_array
+  new_array
   end
+
+def map_to_no_change(source_array)
+  
