21d743e763b68a10e03f9de35ceebfdd8ae38fac
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-02 20:56:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,9 +53,9 @@ def reduce_to_all_true(source_array)
     i = 0
     while i<source_array.length do
       if source_array[i]
-      return true
+       true
     else
-      return false
+       false
       end
       i+=1
     end
