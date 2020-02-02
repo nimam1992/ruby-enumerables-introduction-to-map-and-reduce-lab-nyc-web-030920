3746c35cc85a96fcb5d97aeb69b98512d6ed9a33
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-02 20:20:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,3 +17,13 @@ def map_to_no_change(source_array)
   end
   new_array
   end
+
+def map_to_double(source_array)
+  i = 0
+  new_array = []
+  while i<source_array.length do
+    new_array << source_array[i]*2
+    i+=1
+  end
+  new_array
+  end
